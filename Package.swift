b70b2b7c266e1cb134aecdc80d970358be6f2420
federico-amura-kenility <federico.amura@kenility.com>
2023-10-03 10:47:01 -0300

    Update v0.0.26

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Common",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-common-wrapper/releases/download/0.0.25/Common.xcframework.zip",
-            checksum: "14b9f2acb7d462f4f67354c90e24d14e4891f1f793fec694ba91b6b32961794b"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/0.0.26/Common.xcframework.zip",
+            checksum: "43fcac1b0801d7c3f06ba256be78b133f3317e16d96bbd84ec0b110b59448c76"
         ),
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-shared/releases/download/0.0.25/shared.xcframework.zip",
-            checksum: "8e82b8225453a9e468844433baad680aa4510a49d2c714334918222083430f69"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-shared/releases/download/0.0.26/shared.xcframework.zip",
+            checksum: "7b8372108ec50d4205f5b8bd015c5746546f55558688f0a4385e0df80a29e4c5"
         ),
         .target(
             name: "CommonTargets",
