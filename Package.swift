9cda279bebb14e1af37fdc5f8ff1f88c8cf3106e
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-10-05 15:42:15 +0000

    Update 0.0.32

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-shared/releases/download/0.0.31/shared.xcframework.zip",
-            checksum: "a173d9ef714d443074711cfcb5f60195795afcad681336fa6d468fa8a212fb1e"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-shared/releases/download/0.0.32/shared.xcframework.zip",
+            checksum: "00d34a4f0cd0c1a2ebbd4ef465beb983331660e6e4d03b43ae1a843e8cce37d7"
         ),
         .target(
             name: "CommonTargets",
